ac4c2c0c5489817eb95aa81811c8b95239e66183
Jonathan Peters <peters@lucasware.com>
2017-08-27 23:45:24 -0400

    added delete category route

--- routes/categories.js
+++ routes/categories.js
@@ -6,7 +6,7 @@ let mongoUrl = "mongodb://localhost:27017/count";
 let categoriesCollectionName = "categories";
 
 /**
- * GET all categories in the system
+ * retrieve all categories in the system
  */
 
 router.get('/categories', handleGetCategories);
@@ -18,7 +18,7 @@ async function handleGetCategories(req, res, next) {
 }
 
 /**
- * POST a new category
+ * create a new category
  */
 
 router.post('/category', handlePostCategory);
@@ -28,6 +28,28 @@ async function handlePostCategory(req, res, next) {
     let categoriesCollection = await db.collection(categoriesCollectionName);
     let result = await categoriesCollection.insert(req.body);
     res.send(result.ops[0]);
+}
+
+/**
+ * delete existing categories. this is done using POST because the endpoint
+ * supports multi-delete as opposed to requiring the client to
+ * initiate many delete requests
+ */
+
+router.post('/categories', handleDeleteCategories);
+
+async function handleDeleteCategories(req, res, next) {
+
+    // need to map string IDs back to mongo types
+    let mongoDbObjectIds = req.body.map(id => mongo.ObjectId(id));
+
+    let db = await mongoClient.connect(mongoUrl);
+    let categoriesCollection = await db.collection(categoriesCollectionName);
+    let result = await categoriesCollection.deleteMany({ _id: {
+        $in: mongoDbObjectIds
+    }});
+
+    res.send(result);
 }
 
 module.exports = router;
