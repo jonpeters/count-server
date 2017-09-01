1dbc2ba8a11802eb709196d04ca634bfe0f94b58
Jonathan Peters <peters@lucasware.com>
2017-09-01 00:41:26 -0400

    first stab at a GET time-series method

--- routes/categories.js
+++ routes/categories.js
@@ -77,10 +77,55 @@ async function handleIncrementCategoryCount(req, res, next) {
     // add the actual instant
     instantsCollection.insertOne({
         category_id: categoryIdAsMongoDbObjectId,
-        unixTimestamp: Date.now()
+        unix_timestamp: Date.now()
     });
 
     res.send(result.value);
+}
+
+/**
+ * retrieve instants data
+ */
+
+router.get('/time-series', handleGetTimeseries);
+
+async function handleGetTimeseries(req, res, next) {
+    let oneHourInMs = 60*60*1000;
+
+    let start = req.query.start;
+    // truncate to hour
+    start = start - (start % oneHourInMs);
+
+    let end = req.query.end;
+    // truncate to hour then add 1 hour
+    end = end - (end % oneHourInMs) + oneHourInMs;
+
+    let categoryIdAsMongoDbObjectId = mongo.ObjectId(req.query.category_id);
+
+    let db = await mongoClient.connect(mongoUrl);
+    let instantsCollection = await db.collection(instantsCollectionName);
+
+    let cursor = await instantsCollection.find({ $and: [{ "category_id": categoryIdAsMongoDbObjectId }, { "unix_timestamp": { $gte: start }}, { "unix_timestamp": { $lt: end }} ] });
+
+    // store count per hour, keyed by hour
+    let hash = {};
+
+    // perform in-memory aggregation
+    while (await cursor.hasNext()) {
+        let instantDoc = await cursor.next();
+        // round to hour
+        let value = instantDoc.unix_timestamp - (instantDoc.unix_timestamp % oneHourInMs);
+        // add the hash key, if does not exist
+        if (!hash[value]) hash[value] = 0;
+        // increment
+        hash[value]++;
+    }
+
+    let result = [];
+
+    Object.keys(hash).forEach(key => result.push({ unix_timestamp: parseInt(key), value: hash[key] }));
+
+    res.send(result);
 }
 
 module.exports = router;
