b6778a36eff16c390665ad2065330f474a850f59
Jonathan Peters <peters@lucasware.com>
2017-10-06 11:55:07 -0400

    add test for new category and increment category

--- test/test-categories.js
+++ test/test-categories.js
@@ -6,8 +6,34 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../app');
 var should = chai.should();
+var expect = chai.expect;
 
 chai.use(chaiHttp);
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
+
+const TOKEN = 'x-access-token';
+
+function doCreateNewCategory(name, cb, done, token) {
+    chai.request(app)
+        .post('/api/secure/category')
+        .send({ name: name })
+        .set(TOKEN, token)
+        .end((err, res) => {
+            res.should.have.status(200);
+
+            // retrieve the new category
+            chai.request(app)
+                .get('/api/secure/categories')
+                .set(TOKEN, token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an('array');
+                    res.body.should.contain.something.like({ name: name, count: 0 });
+                    cb(done, res);
+                });
+        });
+}
 
 describe('Categories', () => {
 
@@ -18,6 +44,8 @@ describe('Categories', () => {
         mongo.MongoClient.connect(config.mongoURI[app.settings.env], (err, resultDb) => {
             db = resultDb;
             db.collection("users").drop();
+            db.collection("categories").drop();
+            db.collection("instants").drop();
 
             chai.request(app)
                 .post('/api/sign-up')
@@ -29,16 +57,43 @@ describe('Categories', () => {
         });
     });
 
-    /**
-     * TODO this is temporary just to ensure the token works
-     */
-    it('should pass', (done) => {
-        chai.request(app)
-            .get('/api/secure/categories')
-            .set('x-access-token', token)
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
+    it('should create a new category', (done) => {
+       doCreateNewCategory("a_brand_new_test_category", (d) => d(), done, token);
     });
+
+    it('should increment a category', (done) => {
+        doCreateNewCategory("increment_test_category", (done, res) => {
+            let category = res.body.filter(c => c.name == 'increment_test_category')[0];
+
+            db.collection("categories")
+                .find({ _id: mongo.ObjectId(category._id) })
+                .toArray()
+                .then(category => {
+                    expect(category).to.contain.something.like({ count: 0 });
+                })
+                .then(() => {
+                    chai.request(app)
+                        .post('/api/secure/increment-category-count/' + category._id)
+                        .set(TOKEN, token)
+                        .end((err, res) => {
+                            res.should.have.status(200);
+
+                            db.collection("categories")
+                                .find({ _id: mongo.ObjectId(category._id) })
+                                .toArray()
+                                .then(category => {
+                                    expect(category).to.contain.something.like({ count: 1 });
+                                })
+                                .then(() => db.collection("instants").find({ category_id: mongo.ObjectId(category._id) }).toArray())
+                                .then(instants => {
+                                    expect(instants).to.have.lengthOf(1);
+                                })
+                                .then(done)
+                                .catch(done);
+                        });
+                })
+                .catch(done);
+        }, done, token);
+    });
+
 });
